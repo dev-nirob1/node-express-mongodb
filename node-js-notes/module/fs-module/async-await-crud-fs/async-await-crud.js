4d5a2443bd0e0aa7e38d7155daa4d668e99067ee
dev-nirob1 <dev.nirob1@gmail.com>
2025-01-28 00:11:02 +0600

    async-await crud

---
+++ node-js-notes/module/fs-module/async-await-crud-fs/async-await-crud.js
@@ -0,0 +1,68 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+const fileName = 'result.txt'
+const filePath = path.join(__dirname, fileName)
+
+
+// read directory using promises
+const directory = __dirname;
+
+// fs.readdir(directory).then(data => console.log(data)).catch(err => console.log(err))
+
+// // using async-await
+// const readFolder = async () => {
+//    try {
+//    const res  = await fs.readdir(directory)
+//    console.log(res);
+//    } catch (error) {
+//     console.log(error);
+//    }
+// }
+// readFolder();
+
+
+// create file
+// const createFile = async()=>{
+//     try {
+//         const res = await fs.writeFile(filePath, 'Initial Data', 'utf-8')
+//         console.log('Data added successfully');
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// createFile()
+
+
+//readFile
+// const readFile = async () => {
+//     try {
+//         const res = await fs.readFile(filePath, 'utf-8')
+//         console.log(res);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// readFile()
+
+// update file 
+
+// const updateContent = async()=> {
+//     try {
+//         fs.appendFile(filePath, '\n new content', 'utf-8');
+//         console.log('content updated successfully');
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// updateContent();
+
+// const deleteFile = async()=> {
+//     try {
+//         fs.unlink(filePath);
+//         console.log('folder deleted successfully');
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// deleteFile()
